0701a1802daf85d98d8da25dc515892d6a916f7c
Valeryiar <43443600+Valeryiar@users.noreply.github.com>
2018-11-03 22:04:01 +0300

    46

--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.TestBase;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -16,7 +17,7 @@ public class GroupCreationTests extends TestBase {
 
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before =app.getGroupHelper(). getGroupList();
-        GroupData group=new GroupData("test1", null, null);
+        GroupData group=new GroupData("test2", null, null);
         app.getGroupHelper().initGroupCreation();
         app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupCreation();
@@ -25,13 +26,8 @@ public class GroupCreationTests extends TestBase {
         Assert.assertEquals(after.size(), before.size()+1);
 
 
-        int max=0;
-        for (GroupData g: after){
-            if(g.getId()>max) {
-                max =g.getId();
-            }
-        }
-        group.setId(max);
+
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
 
