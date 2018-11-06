83ef639ba5370625d37aeee51218b5bf8aac8a6b
Valeryiar <43443600+Valeryiar@users.noreply.github.com>
2018-11-06 15:35:21 +0300

    52

--- addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -3,14 +3,14 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
-import ru.stqa.pft.addressbook.model.ContactData;
+
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public class GroupHelper extends HelperBase{
+public class GroupHelper extends HelperBase {
 
     public GroupHelper(FirefoxDriver wd) {
         super(wd);
@@ -40,8 +40,8 @@ public class GroupHelper extends HelperBase{
         click(By.name("delete"));
     }
 
-    public void selectGroup(int index) {
-        wd.findElements(By.name("selected[]")).get(index).click();
+       public void selectGroupById(int id) {
+        wd.findElement(By.cssSelector("input[value='"+id+"']")).click();
 
     }
 
@@ -62,35 +62,41 @@ public class GroupHelper extends HelperBase{
         submitGroupCreation();
         returntoGroupPage();
     }
-    public void modify(int index, GroupData group) {
-        selectGroup(index);
+
+    public void modify(GroupData group) {
+        selectGroupById(group.getId());
         initGroupModification();
         fillGroupForm(group);
         submitGroupModification();
         returntoGroupPage();
     }
-    public void delete(int index) {
-        selectGroup(index);
+
+
+    public void delete(GroupData group) {
+        selectGroupById(group.getId());
         deleteGroup();
         returntoGroupPage();
     }
+
 
     public boolean isThereAGroup() {
         return isElementPresent(By.name("selected[]"));
     }
 
     public int getGroupCount() {
-       return wd.findElements(By.name("selected[]")).size();
+        return wd.findElements(By.name("selected[]")).size();
     }
 
-    public List<GroupData> list() {
-        List<GroupData> groups = new ArrayList<GroupData>();
+
+    public Set<GroupData> all() {
+        Set<GroupData> groups = new HashSet<GroupData>();
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for (WebElement element : elements) {
             String name = element.getText();
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
             groups.add(new GroupData().withId(id).withName(name));
         }
-            return groups;
-        }
+        return groups;
     }
+
+}
